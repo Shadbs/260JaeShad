ee86c0c076270fd043a7c8b4e4866b0f04945bda
Shadbs <sbstanger@gmail.com>
2014-05-09 17:08:07 -0600

    intital commit

---
+++ bestTwoYears/src/besttwoyears/model/mission.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package besttwoyears.model;
+
+import java.util.Objects;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Shad
+ */
+public class mission implements Serializable {
+    private String missionName;
+
+    public String getMissionName() {
+        return missionName;
+    }
+
+    public void setMissionName(String missionName) {
+        this.missionName = missionName;
+    }
+
+    public mission() {
+    }
+
+    @Override
+    public String toString() {
+        return "mission{" + "missionName=" + missionName + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 73 * hash + Objects.hashCode(this.missionName);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final mission other = (mission) obj;
+        if (!Objects.equals(this.missionName, other.missionName)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
