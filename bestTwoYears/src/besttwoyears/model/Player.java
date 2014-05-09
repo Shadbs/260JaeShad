63557790e22c3633bb0765ce63f00ee396ff0799
Shadbs <sbstanger@gmail.com>
2014-05-09 16:17:25 -0600

    intital commit

---
+++ bestTwoYears/src/besttwoyears/model/Player.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package besttwoyears.model;
+
+import java.util.Objects;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Shad
+ */
+public class Player implements Serializable{ 
+        private String lastName;
+        private String missionTitle;
+
+    public Player() {
+    }
+ 
+        
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMissionTitle() {
+        return missionTitle;
+    }
+
+    public void setMissionTitle(String missionTitle) {
+        this.missionTitle = missionTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "lastName=" + lastName + ", missionTitle=" + missionTitle + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 41 * hash + Objects.hashCode(this.lastName);
+        hash = 41 * hash + Objects.hashCode(this.missionTitle);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.lastName, other.lastName)) {
+            return false;
+        }
+        if (!Objects.equals(this.missionTitle, other.missionTitle)) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+    
+}
