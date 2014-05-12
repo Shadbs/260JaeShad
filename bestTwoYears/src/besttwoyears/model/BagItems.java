76df39c324595e81823813186b9abbdce3070de4
zzaewon <zzaewon@hotmail.com>
2014-05-12 16:40:02 -0600

    Added BagItems.java to the model

---
+++ bestTwoYears/src/besttwoyears/model/BagItems.java
@@ -0,0 +1,72 @@
+/*
+ * BagIetms Class
+ */
+
+package besttwoyears.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * @author Jae Lee
+ */
+public class BagItems implements Serializable{
+    
+    // class instance variable
+    private String description;
+    private String name;
+
+    public BagItems() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "BagItems{" + "description=" + description + ", name=" + name + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.description);
+        hash = 79 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BagItems other = (BagItems) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+}
