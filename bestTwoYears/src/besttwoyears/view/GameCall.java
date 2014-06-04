fa6e3aa8f002e1ee297a5646314a07d4141fc9a4
zzaewon <zzaewon@hotmail.com>
2014-06-04 15:30:56 -0600

    Add HelpMenuView Finished

---
+++ bestTwoYears/src/besttwoyears/view/GameCall.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ */
+
+package besttwoyears.view;
+
+/*
+ * @author Jae Lee
+ */
+
+public class GameCall {
+    public void displayMenu() {
+        System.out.println("\n"
+                +"\n *******************************************************"
+                +"\n ------Call Menu displayMenu stub function called-------"
+                +"\n    C - Make a Phone Call                               "
+                +"\n    R - Receive Phone Call                              "
+                +"\n    T - Text to others                                  "
+                +"\n *******************************************************");
+    }   
+}
--- bestTwoYears/src/besttwoyears/view/HelpMenuView.java
+++ bestTwoYears/src/besttwoyears/view/HelpMenuView.java
@@ -16,7 +16,7 @@ public class HelpMenuView {
             + "\n-------------------------------------"
             + "\n|             Help Menu             |"
             + "\n-------------------------------------"
-            + "\n G - Goal of the Game"
+            + "\n G - Goal of the Game                "
             + "\n M - How To Move                     "
             + "\n A - Making & Keep Appoinments       "
             + "\n P - Understanding the Planner       "
@@ -27,7 +27,7 @@ public class HelpMenuView {
     public void displayMenu() {
         char selection = ' ';
         do {
-            System.out.println(HelpMENU);       // Display MainMenu
+            System.out.println(HelpMENU);       // Display HelpMenu
 
             String input = this.getInput();     // get user's selection & See "getInput()"
             selection = input.charAt(0);        // get first character of string
@@ -61,30 +61,54 @@ public class HelpMenuView {
 
         switch (playerChoice) {
             case 'G':// Goal of the Game, Display for direction
-                People.toString(besttwoyears.getCurrrent);
-                gameGoal.displayGoal();
+                System.out.println(
+                        "\n"
+                        +"\n*************************Goal of Your Mission****************************"
+                        +"\n There will be choices you have to make in order to fulfill your mission." 
+                        +"\n Some of the challenges you will face will be deciding what to do        " 
+                        +"\n with your day, who you’re going to go visit, keeping appointments,      "  
+                        +"\n dealing with unruly companions, managing your funds, serving others,    "
+                        +"\n and strengthening your own spiritual strength. It is important to keep  "
+                        +"\n your spiritual strength up and your pride down. Doing the work of the   " 
+                        +"\n Lord will increase your spirit while not doing what you are supposed    "
+                        +"\n to do your pride will go up and your spirit will go down. If your pride "
+                        +"\n gets to high you may be sent to the mission president to be humbled.    "
+                        +"\n or sent home. Can you survive 2 years?                                  "
+                        +"\n*************************************************************************");
                 break;
+                
             case 'M': // How to Move, Display for direction
-                GameMove gameMove = new GameMove();
-                gameGoal.displayMove();
+                    System.out.println("\n"
+                        +"\n*****************How to Move***********************"
+                        +"\n Do Your Work and Find the God's chilren, we will  "
+                        +"\n transfer your mission area once investigators     "
+                        +"\n are converted. Go, Find and get them!             "
+                        +"\n***************************************************");
                 break;
-            case 'C':// Making & Keep Appoinments
-                Call.toString();
+                
+            case 'C':// Making & Receive Phonecall - GamaCall.java
+                GameCall gameCall = new GameCall();
+                gameCall.displayMenu();
                 break;
-            case 'P':// Understanding the Planner
+                
+            case 'P':// Understanding the Planner - connected to GamePlanner.java
                 GamePlanner gamePlanner = new GamePlanner();
                 gamePlanner.displayMenu();
                 break;
-            case 'T': // Understanding P-Day Task 
-                prepDay.prepDay(BestTwoYears.getCurSrentGame());
+                
+            case 'T': // Understanding P-Day Task - connected to GamePrepDay.java
+//                prepDay.prepDay(BestTwoYears.getCurrentGame());
+                GamePrepDay gamePrepDay = new GamePrepDay();
+                gamePrepDay.displayMenu();
                 break;
-            case 'E': // Understanding P-Day Task 
+                
+            case 'E': // Back to the MainMenuView.java
                 MainMenuView mainMenu = new MainMenuView();
                 mainMenu.displayMenu();
                 break;
 
             default:
-                System.out.println("***Please Choose Option again***");
+                System.out.println("***Please Choose Option Again***");
                 break;
         }
 
