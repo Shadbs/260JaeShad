dbc7c1b7d730302dfd24b4ee7bdedca6c073581a
zzaewon <zzaewon@hotmail.com>
2014-06-30 15:55:54 -0600

    Try it

--- bestTwoYears/src/besttwoyears/view/GamePlanner.java
+++ bestTwoYears/src/besttwoyears/view/GamePlanner.java
@@ -17,12 +17,12 @@ public class GamePlanner extends View {
 
     public GamePlanner() {
         super("\n"
-                + "\n-------------------------------------"
-                + "\n|             Planner Help           |"
-                + "\n-------------------------------------"
+                + "\n-----------------------------------------"
+                + "\n|             Planner Help              |"
+                + "\n-----------------------------------------"
                 + "\n P - Using the planner and how to use it"
                 + "\n E - Exit to Help Menu"
-                + "\n-------------------------------------");
+                + "\n-----------------------------------------");
     }
 
     @Override
@@ -31,13 +31,14 @@ public class GamePlanner extends View {
         switch (playerChoice) {
             case 'P': //display Help Menu
                 System.out.println("\n"
-                        + "\n**********What Is The Planner?***********"
-                        + "\n"
+                        + "\n*****************What Is The Planner?*********************************************"
+                        + "\n                                                                                  "
                         + "\n When selecting the Planner, it will display any future appointments"
                         + "\n that you will have that day. It will also have the choice between"
                         + "\n adding a new appointment or deleting one. It is up to the missionary"
                         + "\n to keep those appointments to keep from facing the consequnses of missing them."
-                        + "\n ****************Using The Planner*********************"
+                        + "\n                                                                                  "
+                        + "\n ****************Using The Planner************************************************"
                         + "\n"
                         + "\n After the planner opens it will automatically display the appointements of the day."
                         + "\n A - Using this command you will be prompted to add the name, address, and phone number of the"
